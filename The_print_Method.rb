41348c13eeeececd0ffee67f88894d841189813d
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-19 20:07:19 -0700

    The print method

---
+++ The_print_Method.rb
@@ -0,0 +1,13 @@
+puts "Hello"
+puts "World"
+
+# Print does not create a linebreak at the end like puts does
+print "Hello"
+print "World"
+puts
+print "Hello "
+print "World"
+puts
+print "Hello"
+print " "
+print "World"
