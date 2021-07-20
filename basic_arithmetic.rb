1bb4ad8d01170f4844e2f910001f2204caf1b40f
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-19 20:49:54 -0700

    add basic arithmetic

---
+++ basic_arithmetic.rb
@@ -0,0 +1,20 @@
+p 1 + 4
+p 1 - 6
+p 5 * 7
+
+p "School" + "bus"
+
+p 10 / 5
+p 12 / 5
+p 12.0 / 5
+p 0.5 / 3
+p -0.75 / 3
+
+p 2 * 2 * 2 * 2
+p 2 ** 4
+
+# this gives you the remainder
+p 5 % 2
+p 6 % 3
+p 15 % 3
+p 23 % 4
