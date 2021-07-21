65d076540a48a15982d94fc0064f37e0e8d071cf
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-21 07:07:25 -0700

    intro to booleans

---
+++ intro_to_booleans.rb
@@ -0,0 +1,11 @@
+# Booleans are true or false. that’s it. the validity of  a statement
+p 5 < 10
+p 10 > 12
+
+age = 25
+handsome = true
+stupid = false
+
+# They’re different classes but classified as a larger set of booleans
+p handsome.class
+p stupid.class
