1ce6e90216bf081b818a8953fd7de782a5c72b8f
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-20 20:48:06 -0700

    add constants

---
+++ constants.rb
@@ -0,0 +1,19 @@
+=begin
+- constants acts similar to a variable, similar structure
+- represents somethings that should not change overtime or the course of the program
+- constants are required to start with a capital letter.
+- best practice is to write in complete uppercase.
+=end
+
+age = 25
+p age
+
+age = 30
+p age
+
+NAME = "Nelson"
+PI = 3.14150
+
+p NAME
+p PI
+p "My name is #{NAME}"
