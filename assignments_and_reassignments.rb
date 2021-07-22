329e7a11a3588a5ce521a7c95959c733ddda94a3
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-22 14:44:56 -0700

    add assignments and reassignments

---
+++ assignments_and_reassignments.rb
@@ -0,0 +1,46 @@
+puts "add"
+a = 10
+a = a + 5
+p a
+# These syntaxes are exactly the same. same result
+a += 5
+p a
+puts
+
+puts "subtract"
+b = 100
+p b
+# b = b - 50
+b -= 50
+p b
+puts
+
+puts "multiply"
+c = 3
+# c = * 4
+c *= 4
+p c
+puts
+
+puts "divide"
+d = 5
+p d
+# d = d / 2
+d /= 2
+p d
+puts
+
+puts "modulo - remainder"
+e = 230
+p e
+# e = e % 3
+e %= 3
+p e
+puts
+
+puts "square root"
+f = 50
+p f
+# f = f ** 3
+f **= 3
+p f
