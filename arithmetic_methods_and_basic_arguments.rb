a2ba48c82fbf92a6732567dbeb690ce18dfe8560
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-21 21:24:17 -0700

    arithmetic methods and basic arguments

---
+++ arithmetic_methods_and_basic_arguments.rb
@@ -0,0 +1,36 @@
+=begin
+The parenthesis is optionla in ruby vs other languages
+p 1.next
+vs
+p 1.next()
+Usually in ruby we use () when there's an input or arguments
+=end
+
+p 1 + 2
+# () it's an argument or input. The () is not required though
+p 1.+(2)
+p 1.+ 2
+puts
+
+p 10 - 5
+p 10.-(5)
+puts
+
+p 3 * 4
+p 3.*(4)
+puts
+
+p 10 / 5
+p 10./(5)
+p 10.div(5)
+puts
+
+p 12.0 / 5.0
+puts
+
+p 10 % 3
+p 10.%(3)
+p 10.modulo(3)
+puts
+
+p 2 ** 6
---
+++ less_than_greater_than_operators.rb
@@ -0,0 +1,19 @@
+p 1 < 8
+p 10 < 15
+puts
+
+p 12 < 5
+puts
+
+#This will give you less than or equal to
+p 1 <= 5
+p 5 <= 5
+p 5 < 5
+puts
+
+p 12.86 < 21.13
+p 12.43 > 23.43
+puts
+
+p 8 >= 5
+p 8 >= 10
