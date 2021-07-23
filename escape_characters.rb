81bf02ce0b4a6f4e0f38d48da28b8a91ed85f9d7
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-22 18:03:44 -0700

    add escape characters

---
+++ escape_characters.rb
@@ -0,0 +1,23 @@
+puts "Hello World"
+puts 'Hello World'
+puts
+
+puts "Julie said 'Goodbye' to Romeo"
+puts 'Juliet said "Goodbye" to Romeo'
+puts
+
+puts "Juliet said \"Goodbye\" to Romeo"
+puts 'Juliet said \'Goodbye\' to Romeo'
+puts
+
+# \n is the syntax for a line break
+puts "Let's add a line break\nright here"
+puts
+result = "Let's add a line break\nright here"
+puts result
+puts
+p result
+puts
+
+# \t inserts a tab
+puts "\tInset a tab right here"
