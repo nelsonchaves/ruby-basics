6b2833a2ac69623c59b38d0dee39727445364113
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-22 17:48:23 -0700

    create a string

---
+++ create_a_string.rb
@@ -0,0 +1,29 @@
+puts "Hi there, I'm a string"
+puts "I can also include charcaters and numbers: # $ ! @ 5 9"
+puts
+
+name = "Nelson"
+revenue = "$23 dollars"
+puts name, revenue
+puts
+
+space = " "
+p space
+p space.length
+puts
+
+empty = ""
+p empty
+p empty.length
+puts
+
+p name.class
+puts
+
+# the most community accepted standard you the () though you dont have to
+# String.new()
+name = String.new("Nelson")
+p name
+puts
+
+p 5.to_s
