14ce7a3fff1bf9a95d272e773d62b4ed392ff3d4
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-25 20:49:34 -0700

    add case methods

---
+++ case_methods.rb
@@ -0,0 +1,24 @@
+# capitalize method will capitalize only the first letter of the string and everything else will be converted to lowercase
+# capitalize understand that everything is proper even if the words are messed up
+# number will be ignored
+puts "hello".capitalize
+puts "heLLo".capitalize
+puts "Hello".capitalize
+puts "Hello123".capitalize
+puts "Hello World".capitalize
+puts
+
+puts "nelson123".upcase
+puts "blah blah blah".upcase
+puts
+
+lowercase = "I'm patient"
+uppercase = lowercase.upcase
+p uppercase
+puts
+
+p "NRLDON@#!@$()".downcase
+puts
+
+# swapcase just reverts everything
+p "NeLsOn".swapcase
