c0aef93bc80c140bdce4cf518afe5c3d9b51302b
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-25 20:53:47 -0700

    reverse method on a string

---
+++ reverse_method_on_a_string.rb
@@ -0,0 +1,10 @@
+# The reverse method reverses a string
+# flips it around. makes it read back to front
+
+puts "Ruby".reverse
+puts "12345".reverse
+puts "!@{$%}".reverse
+puts "!@{$%}".reverse.class
+
+# This is how method chaining operates
+puts "ruby is fun".upcase.reverse.downcase
