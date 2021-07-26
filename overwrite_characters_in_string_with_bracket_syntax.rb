65fa308170f9b500e8dd7935f103a5bdbc99d2c9
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-25 20:38:10 -0700

    add overwrite characters in string

---
+++ overwrite_characters_in_string_with_bracket_syntax.rb
@@ -0,0 +1,32 @@
+thing = "Rocket Ship"
+p thing
+p thing[0]
+puts
+
+thing[0] = "p"
+p thing
+puts
+
+thing[1] = "a"
+p thing
+puts
+
+thing[9] = "o"
+p thing
+puts
+
+fact = "I love blueberry pie"
+p fact
+puts
+
+fact[7, 4] = "rasp"
+p fact
+puts
+
+fact[7..10] = "blue"
+p fact
+puts
+
+fact[2..5] = "absolutely adore"
+p fact
+puts
