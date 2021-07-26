c3a28413afe5191047a680d9bdb513a902aa2a69
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-25 18:27:10 -0700

    add length & size methods

---
+++ length_and_size_methods.rb
@@ -0,0 +1,18 @@
+a = "Hello world"
+b = "Hi, my name is Nelson!"
+c = " "
+d = ""
+
+p a.length
+p b.length
+p c.length
+p d.length
+puts
+
+p a.size
+p b.size
+p c.size
+p d.size
+puts
+
+p a.length.class
