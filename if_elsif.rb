9be270c9bc94e510dbf35dba2a661231a3e7bc64
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-26 20:10:28 -0700

    add if elsif

---
+++ if_elsif.rb
@@ -0,0 +1,20 @@
+color = "Green"
+
+if color == "Red"
+  puts "red is rad"
+elsif color == "Green"
+  puts "Green is great!"
+elsif color == "Yellow"
+  puts "Yay for yellow!"
+end
+puts
+
+number = 55
+
+if number < 25
+  puts "That's a low number!"
+elsif number < 50
+  puts "That's a number in the middle!"
+elsif number < 75
+  puts "That's a big number"
+end
