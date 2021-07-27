bb8cffe391a4f06f79ed6e8ccb4cab9068cc6601
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-26 20:38:21 -0700

    add multiple conditions with ||

---
+++ multiple_conditions_with_||.rb
@@ -0,0 +1,9 @@
+budget = 11
+price = 10
+mood = "Joyful"
+
+if budget < price || mood == "Happy"
+  puts "I'm going to buy the item"
+elsif budget > 10 || mood == "Sad"
+  puts "I'm going ot eat ice cream"
+end
