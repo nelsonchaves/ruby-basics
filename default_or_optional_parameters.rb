3f1091737476475aae56fd8bd1935fd01a12c86b
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-27 20:58:05 -0700

    typo

--- default_or_optional_parameters.rb
+++ default_or_optional_parameters.rb
@@ -8,5 +8,3 @@ make_phone_call(1239402, 4)
 
 # By the setting the numbers in the arguments we made them default values
 # They are optional if a person does input a value. It will fall back to the default
-puts "Call A Method from Another Method or Interpolated String".downcase
-call_a_method_from_another_method_or_interpolated_string
