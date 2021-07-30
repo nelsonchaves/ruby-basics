7e1f3d9a71f8a7caca765a1f279997bac5b0be81
Nelson Chaves <nelsonchavespro@gmail.com>
2021-07-30 08:50:56 -0700

    add FizzBuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+	i = 1
+	until i > number
+		if i % 15 == 0
+		puts "FizzBuzz for #{i}"
+		elsif i % 3 == 0
+			puts "Fizz for #{i}"
+		elsif i % 5 == 0
+			puts "Buzz for #{i}"
+		end
+		i += 1
+	end
+end
+
+fizzbuzz(45)
