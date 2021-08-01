236e9491a6ad758e5050dfd31a2742791daa2ca6
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-01 01:25:59 -0700

    add range size method

---
+++ range_size_method.rb
@@ -0,0 +1,8 @@
+numbers = 143..769
+p numbers.size
+
+=begin
+- this size method gives us back the total number of items or elements within the sequence within this collection, within this interval that we call a range.
+- So between these two numbers, there are a total of 627 values.
+- Keep in mind that the two dots are the three dots make a difference in this case.
+=end
