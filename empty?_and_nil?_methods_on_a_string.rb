a7e4a5fbb8d1cad274a32160eca5638779d2d742
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-02 21:57:58 -0700

    add arrays empty? and nil? methods on a string

--- empty?_and_nil?_methods_on_a_string.rb
+++ empty?_and_nil?_methods_on_a_string.rb
@@ -26,3 +26,27 @@ last_name = name[3, 4]
 p last_name
 p last_name.nil?
 puts
+
+puts [1, 2, 3].empty?
+# false because an array of 3 elements is not empty
+puts [].empty?
+puts [].length == 0
+puts [false, false, false].empty?
+puts [nil, nil].empty?
+# - It's not testing how many of the elements are truthy or true. It's simply checking for the presence of one or more elements.
+puts
+
+puts [false, false, false].nil?
+puts 1.nil?
+puts 3.14.nil?
+puts
+
+letters = ("a".."j").to_a
+character = letters[5]
+p character
+p character.nil?
+puts
+
+character = letters[25]
+p character
+p character.nil?
