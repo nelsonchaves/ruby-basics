c22c365b1d501db81774989463c43e10dd173fb0
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-02 21:36:56 -0700

    add length, size and count method

--- length_and_size_methods.rb
+++ length_and_size_methods.rb
@@ -16,3 +16,28 @@ p d.size
 puts
 
 p a.length.class
+puts
+
+# when called on an array, the Lent method returns the total number of elements in the array. even nil values which are also objects just empty
+p [1, 2, 3, 4, "Hello", 3.14, nil].length
+p [1, 2, 3, 4, "Hello", 3.14, nil].size
+# this method is basically the same thing
+puts
+
+p [].length
+p [].size
+puts
+
+p [1, 2, 3, 4, "Hello", 3.14, nil].count
+# the count method is a little bit more expansive. You can provide an argument that represents what it is that you want it to count.
+puts
+
+p [1, 2, 3, 4, 2, "Hello", 2, 3.14, nil].count(2)
+puts
+
+p [true, true, false, false, false, false, false, true].count(true)
+p [true, true, false, false, false, false, false, true].count(false)
+p [true, true, false, false, false, false, false, true].count(nil)
+p [true, true, false, false, false, false, false, true].count("hello")
+
+# you can see count is a little bit more flexible. It allows you to actually specify an optional argument and it's going to count how many times that argument appears in the original array.
