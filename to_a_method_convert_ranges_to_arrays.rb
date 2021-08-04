36e9df757c41f4210d8d5035daff15f3a3cea2eb
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-03 21:17:10 -0700

    add to_a method convert ranges to arrays

---
+++ to_a_method_convert_ranges_to_arrays.rb
@@ -0,0 +1,13 @@
+letter_range = "A".."T"
+p letter_range
+letters_array = letter_range.to_a
+p letters_array.class
+p letters_array[10]
+
+numbers_range = 415..450
+p numbers_range
+numbers_array = numbers_range.to_a
+p numbers_array.size
+p numbers_array[0]
+p numbers_array[-1]
+p numbers_array[10..15]
