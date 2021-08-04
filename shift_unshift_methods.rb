a78dfc4434135eeeeb59bc0e09fd7153da992c31
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-03 19:24:31 -0700

    add shift and unshift method

---
+++ shift_unshift_methods.rb
@@ -0,0 +1,21 @@
+=begin
+- the complementary methods which operate on the beginning of the array
+- Shift basically does the exact same thing as Pop does just to the beginning of the array.
+- it removes the very first element.
+- the shift method returns the value that it removed.
+- You can also save the extracted element to a variable
+=end
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p arr
+extract = arr.shift(2)
+p arr
+p extract
+
+# What the unshift method does is add an element to the beginning of the array.
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p arr
+arr.unshift(25)
+p arr
+arr.unshift(100, 500, 1000)
+p arr
