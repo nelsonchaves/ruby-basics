7d26e1fa14b5bf97ebbe280e33ccede6e2a5024e
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-04 22:17:13 -0700

    add block review

---
+++ blocks_review.rb
@@ -0,0 +1,8 @@
+3.times { puts "Hello there" }
+
+3.times { |number| puts "I am currently on loop number #{number}" }
+
+5.times do |number|
+	square = number * number
+	puts "The current number is #{number} and its square is #{square}"
+end
