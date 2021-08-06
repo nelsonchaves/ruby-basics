8c751e7d8346d94e394b7ba23331dac740d9d49c
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-05 23:18:53 -0700

    iteration over array with while or until loops

---
+++ iteration_over_array_with_while_or_until_loops.rb
@@ -0,0 +1,21 @@
+# This method "animal.length" allows it to dynamically change, this is going to be whatever number I need to iterate up to.
+# And as a result, I'm always going to get all of the elements covered.
+
+animals = ["Lion", "Zebra", "Baboon", "Cheetah", "Kangaroo"]
+
+i = 0
+while i < animals.length
+	puts i
+	puts animals[i]
+	i += 1
+end
+puts
+
+# until is going to go until something is true.
+# So what I need to do is go until "i" is going to be equal to the length of the animal's array.
+i = 0
+until i == animals.length
+	# puts i
+	puts animals
+	i += 1
+end
