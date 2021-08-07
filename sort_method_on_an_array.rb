aed5cb34b8d0e4825835a915cad120c34ea743fc
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-07 01:49:55 -0700

    typo

--- sort_method_on_an_array.rb
+++ sort_method_on_an_array.rb
@@ -3,16 +3,19 @@ words = ["caterpillar", "kangaroo", "apple", "Zebra"]
 p numbers
 p words
 puts
+
 # If I do numbers that sort, You can see here I'm going to get the numbers in ascending order.
 p numbers.sort
 
 # If I call the sort method on an array of strings, I'm simply going to sort them in alphabetical
 p words.sort
 puts
+
 # if I replace the zebra with a Z, but a capital Z, this would actually be sorted first. all capital letters, no matter what they are, come before all lowercase letters.
 p numbers.sort!
 p words.sort!
 puts
+
 =begin
 - sort method does have an equivalent bang method.
 - a bang method is something that mutates the original variable or the object of the variable is pointing to.
