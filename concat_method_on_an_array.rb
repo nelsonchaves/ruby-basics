3e333ba2fb89427f8b9e4de4aab869fce48e26ac
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-07 10:50:22 -0700

    concat method on an array

---
+++ concat_method_on_an_array.rb
@@ -0,0 +1,35 @@
+p [1, 2, 3] + [4, 5]
+p [1, 2, 3].concat([4, 5])
+puts
+
+# concat does modify the original array or object that it's pointing to.
+nums = [1, 2, 3]
+p nums
+nums.concat([4, 5, 6])
+p nums
+puts
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+def custom_concat(arr1, arr2)
+  arr1 + arr2
+end
+
+p custom_concat(a, b)
+p a
+p b
+puts
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+# Using this method the a array becomes mutated
+def custom_concat(arr1, arr2)
+	arr2.each { |elem| arr1 << elem }
+	arr1
+end
+
+p custom_concat(a, b)
+p a
+p b
