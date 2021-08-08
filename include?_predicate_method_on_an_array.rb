b2ff0c434acf2436644122ec600cbc294c295e86
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-07 23:26:16 -0700

    include? in an array

---
+++ include?_predicate_method_on_an_array.rb
@@ -0,0 +1,7 @@
+p "Hello".include?("l")
+
+# include is a predicate method returns true or false If the argument is found in the object that the method is called on
+
+numbers = [1, 2, 3, 4, 5]
+
+p numbers.include?(4)
