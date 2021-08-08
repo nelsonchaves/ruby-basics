ca6d32163126d19ff21c70a457da7dbe42a54e72
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-07 23:36:01 -0700

    add index and find index methods on an array

---
+++ index_and_find_index_methods_on_an_array.rb
@@ -0,0 +1,10 @@
+# the two methods are exactly the same. They are identical.
+# They are two different names for the exact same method.
+
+colors = ["Red", "Blue", "Green", "Red"]
+
+p colors.index("Green")
+p colors.find_index("Red")
+p colors.index("Orange")
+
+p "he .select Method on an Array".downcase
