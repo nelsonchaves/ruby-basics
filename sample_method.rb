11f2c1352583c877a54b5332b08d82270846ca1d
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-30 23:50:28 -0700

    add sample method

---
+++ sample_method.rb
@@ -0,0 +1,6 @@
+# - the sample method. it's used to take one or more random elements from an array.
+flavors = ["Chocolate", "Vanilla", "Strawberry", "Rum Raisin"]
+
+# - So whenever I call sample with an argument, I'm always going to get back an array.
+p flavors.sample
+p flavors.sample(2)
