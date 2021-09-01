ec5f0780e9a0f087253742d86e2beb2997548f41
Nelson Chaves <nelsonchavespro@gmail.com>
2021-08-31 21:21:51 -0700

    add array union combine array and exclude duplicates

---
+++ array_union_combine_array_and_exclude_duplicates.rb
@@ -0,0 +1,12 @@
+# a union combines multiple arrays into one and then removes any duplicates from that final array as well.
+# So the union is really a representation of all of the unique elements across all of the arrays.
+
+p [1, 1, 2, 2, 3, 3, 3] | [3, 4, 4, 5] | [3, 4, 4, 5, 6, 7]
+
+a = [3, 4, 4, 5]
+b = [1, 1, 2, 2, 3, 3, 3]
+
+def custom_union(arr1, arr2)
+  result = []
+  
+end
