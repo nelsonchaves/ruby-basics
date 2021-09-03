e8536255dca7fb7223dd11852c8dffa3c4fa3868
Nelson Chaves <nelsonchavespro@gmail.com>
2021-09-02 21:31:14 -0700

    add hash create and extract values from hashes by their keys

---
+++ hash_create_and_extract_values_from_hashes_by_their_keys.rb
@@ -0,0 +1,18 @@
+nfl_roster = {"Tom Brady" => "New England Patriots",
+          		"Tony Romo" => "Dallas Cowboys",
+          		"Rob Gronkowski" => "New England Patriots",
+          		"Dan Marino" => "Miami Dolphins"}
+
+nba_roster = {"Cleveland Cavaliers" => ["LeBron James", "Kevin Love", "Kyrie Irving"],
+		          "Golden State Warriors" => ["Stephen Curry", "Klay Thompson", "Kevin Durant"]}
+
+p nfl_roster
+p nfl_roster["Dan Marino"]
+puts
+
+p nba_roster
+p nba_roster["Golden State Warriors"]
+puts
+
+p nfl_roster["Donald Duck"]
+p nfl_roster["tom brady"]
