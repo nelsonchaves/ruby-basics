64020e267341c965e13921df1c61949660755fdd
Nelson Chaves <nelsonchavespro@gmail.com>
2021-09-03 23:42:40 -0700

    add hash add key value pair to hash with bracket

---
+++ hash__add_key_value_pair_to_hash_with_bracket.rb
@@ -0,0 +1,18 @@
+menu = {burger: 3.99, taco: 5.96, chips: 0.50}
+
+p menu[:burger]
+
+p menu[:sandwich] = 8.99
+
+p menu
+
+menu[:taco] = 2.99
+
+p menu
+
+# the store method
+menu.store(:sushi, 24.99)
+
+p menu.store(:steak, 34.99)
+p menu
+p menu[:steak]
