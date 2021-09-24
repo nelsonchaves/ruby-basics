e39cc7fa9208dee10a8caea2f48a7aaaedc9f8c4
Nelson Chaves <nelsonchavespro@gmail.com>
2021-09-24 07:37:50 -0700

    word frequency challenge

--- challenge__word_frequency.rb
+++ challenge__word_frequency.rb
@@ -2,13 +2,23 @@
 # the words in the string and the values will represent
 # how many times that key occurs
 
-sentence = "Once upon a time in a land far far away"
+sentence = "Once upon a time in a land far far far away"
 
 def word_count(string)
 	words = string.split(" ")
-	count = {}
-  words.each do |word|
-    count[word]
-	end
+	count = Hash.new(0)
+	words.each { |word| count[word] += 1 }
+	count
 end
+
 p word_count(sentence)
+
+my_hash = {"A" => "An apple", "B" => "Big banana", "C" => "Cool cat"}
+p my_hash.key?(:A)
+
+one = {Washington: 1, Jefferson: 2, Lincoln: 5 }
+two = {Hamilton: 10, Jackson: 20, Lincoln: "5 dollars!" }
+p one.merge(two)
+
+colors = {red: 3, blue: 4, black: 5, orange: 6, blonde: 6}
+p colors.select { |color, number| color.to_s.include? "b" }
